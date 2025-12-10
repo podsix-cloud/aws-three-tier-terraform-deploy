a6024689aa9094bfcb109df4c927335a075a88de
podsix-cloud <mypodsix@gmail.com>
2025-12-10 10:49:24 +0000

    Deploy terraform

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "podsix-s3-bucket01"
+    bucket = "mypodsix-s3-bucket"
     key    = "mypodsix/prodution/terraform.tfstate"
     region = "us-east-1"
   }
