1a4326e2d3118fb318cde46c8f80ac46115f8c05
podsix-cloud <mypodsix@gmail.com>
2025-12-10 10:36:27 +0000

    Provisioning AWS Infrastructure

--- module-vpc/09-public-route-table.tf
+++ module-vpc/09-public-route-table.tf
@@ -1,25 +1,54 @@
-# get the default route table for the VPC
-data "aws_route_table" "default-route-table" {
-  vpc_id = aws_vpc.vpc-main.id
-  filter {
-    name   = "association.main"
-    values = ["true"]
-  }
-}
-# tag the default route table
+# # get the default route table for the VPC
+# data "aws_route_table" "default-route-table" {
+#   vpc_id = aws_vpc.vpc-main.id
+#   filter {
+#     name   = "association.main"
+#     values = ["true"]
+#   }
+# }
+# # tag the default route table
+# resource "aws_ec2_tag" "tag_default_route_table" {
+#   resource_id = data.aws_route_table.default-route-table.id
+#   key         = "Name"
+#   value       = "${var.environment}-public-route-table"
+# }
+# # associate the default route table with the VPC
+# resource "aws_main_route_table_association" "main" {
+#   vpc_id         = aws_vpc.vpc-main.id
+#   route_table_id = data.aws_route_table.default-route-table.id
+# }
+# # add a route to the default route table to allow internet access
+# resource "aws_route" "default-route-table" {
+#     route_table_id = data.aws_route_table.default-route-table.id
+#     destination_cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gw.id
+# }   
+
+# Tag the default route table of the VPC
 resource "aws_ec2_tag" "tag_default_route_table" {
-  resource_id = data.aws_route_table.default-route-table.id
+  resource_id = aws_vpc.vpc-main.default_route_table_id
   key         = "Name"
   value       = "${var.environment}-public-route-table"
+
+  depends_on = [aws_vpc.vpc-main]
 }
-# associate the default route table with the VPC
+
+# Set main route table explicitly (not required, but optional)
 resource "aws_main_route_table_association" "main" {
   vpc_id         = aws_vpc.vpc-main.id
-  route_table_id = data.aws_route_table.default-route-table.id
+  route_table_id = aws_vpc.vpc-main.default_route_table_id
+
+  depends_on = [aws_vpc.vpc-main]
 }
-# add a route to the default route table to allow internet access
-resource "aws_route" "default-route-table" {
-    route_table_id = data.aws_route_table.default-route-table.id
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-}   
+
+# Add route to the Internet Gateway
+resource "aws_route" "default_route" {
+  route_table_id         = aws_vpc.vpc-main.default_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+
+  depends_on = [
+    aws_internet_gateway.gw,
+    aws_vpc.vpc-main
+  ]
+}
--- module-vpc/10-public-route-associate.tf
+++ module-vpc/10-public-route-associate.tf
@@ -1,7 +1,8 @@
 resource "aws_route_table_association" "public_route_table_association" {
   count = true ? var.countsub : 0
   subnet_id      = aws_subnet.public_subnet[count.index].id
-  route_table_id = data.aws_route_table.default-route-table.id
+  # route_table_id = data.aws_route_table.public-route-table.id
+  route_table_id = aws_vpc.vpc-main.default_route_table_id
 
   depends_on = [aws_internet_gateway.gw]
   
